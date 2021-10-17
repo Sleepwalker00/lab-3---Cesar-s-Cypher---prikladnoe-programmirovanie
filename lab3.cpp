0786b5f97d471be13e14e4edf053713649b0f47e
Cyber Fist <90570582+henrypotterindauni@users.noreply.github.com>
2021-10-18 01:42:38 +0500

    Работа над ошибками
    Обновлён алфавит, а также теперь сдвиг не ограничен 33 (в таком случае число преобразуется в остаток от деления на 33)

--- lab3.cpp
+++ lab3.cpp
@@ -1,35 +1,28 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 #include<Windows.h>
 #include<string>
 
 using namespace std;
 
-
 int main()
 {
+    setlocale(LC_ALL, "Russian");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     int i = 0, k;
     char ABC[] =
     {
-                'а','б','в','г','д','е','ё','ж','з','и',
-                'й','к','л','м','н','о','п','р','с',
-                'т','у','ф','х','ц','ч','ш','щ','ъ',
-                'ы','ь','э','ю','я' 
+                "абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя"
     };
     char msg[150];
-    cout << "Введите сообщение на русском языке строчными буквами, которое будет закодировано: " << endl;
+    cout << "Введите сообщение на русском языке строчными буквами, которое будет закодировано шифром Цезаря: " << endl;
     cin.getline(msg, 150);
-    cout << "\nВведите сдвиг:" << endl;
+    cout << "\nВведите сдвиг :" << endl;
     cin >> k;
+    if (k > 33)
+    {
+        k = k % 33;
+    }
     cout << endl;
     char* begin = ABC;
     char* end = ABC + sizeof(ABC);
@@ -38,8 +31,7 @@ int main()
         char* ch = find(begin, end, msg[i]);
         if (ch != end) msg[i] = *(begin + (ch - begin + k) % sizeof(ABC));
     }
-    cout << "Полученное закодированное сообщение: " << endl;
+    cout << "Полученное сообщение, скрытое от лишних умов :" << endl;
     cout << msg << endl;
     return 0;
 }
-
